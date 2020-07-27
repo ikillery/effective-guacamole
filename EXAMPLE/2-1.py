7cec3311b06d0a51d315f833bf11fd20b9296584
chenfeixu <chenfeixu@iauto.com>
2020-07-27 11:51:28 +0800

    update example 2-1.py

--- EXAMPLE/2-1.py
+++ EXAMPLE/2-1.py
@@ -75,3 +75,66 @@ import matplotlib.pyplot as plt
 housing.hist(bins=50, figsize=(20,15))
 save_fig("attribute_histogram_plots")
 # plt.show()
+plt.clf()
+
+np.random.seed(42)
+
+import numpy as np
+
+# For illustration only. Sklearn has train_test_split()
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set = split_train_test(housing, 0.2)
+print(len(train_set))
+print(len(test_set))
+
+import hashlib
+def test_set_check(identifier, test_ratio, hash=hashlib.md5):
+    return bytearray(hash(np.int64(identifier)).digest())[-1] < 256 * test_ratio
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+housing_with_id = housing.reset_index()   # adds an `index` column
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+
+# housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+# train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+
+print(test_set.head())
+
+from sklearn.model_selection import train_test_split
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+housing["median_income"].hist()
+save_fig("median_income_histogram_plots")
+plt.clf()
+
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+housing["income_cat"].value_counts()                               
+housing["income_cat"].hist()
+save_fig("income_cat_histogram_plots")
+plt.clf()
+
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
+
+print(housing["income_cat"].value_counts() / len(housing))
